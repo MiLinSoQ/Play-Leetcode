acc97f4a46ea10106e9f0562595bf4d10484b872
MiLinSoQ <bluejacky258@gmail.com>
2020-10-23 11:12:34 +0800

    0006 solved.

---
+++ 0006-ZigZag Conversion/Solution.java
@@ -0,0 +1,79 @@
+/**
+	Url: https://leetcode.com/problems/zigzag-conversion/
+	Author: MiLinSoQ
+	Time: 2020-10-23
+	Title: ZigZag Conversion
+	No: 6
+*/
+
+/**
+	傳入一個 String (s) & Integer (numRows)
+	將 s 字串以 numRow 為高度轉換排序。
+	
+	EX: 
+		
+		1	A				E
+			
+			↓			↗
+		
+		2	B		D
+	
+			↓	↗
+			
+		3	C
+		
+		
+		Input: s = "ABCDE", numRows = 3
+		Output: "AEBDC"
+	
+*/
+public class Solution {
+	
+	public static void main(String args[]) {
+		
+		String s = "ABCDEFG";
+		int rows = 3;
+		
+		(new Solution()).convert(s, rows);
+		
+		
+		
+	}
+	
+    public String convert(String s, int numRows) {
+		if (numRows <= 1) return s;
+		System.out.println(s + ", " + numRows);
+		
+		String[] tempStrings = new String[numRows];
+		for (int i = 0; i < numRows; i++) tempStrings[i] = "";
+		
+		int r = 1; 
+		boolean isDown = true;
+		for (int i = 0; i < s.length(); i++) {
+						
+			String tempS = s.substring(i, i +1);
+			tempStrings[r - 1] += tempS;
+
+			if (isDown) {
+				r++;
+				isDown = (r != numRows);
+			} else {
+				r--;
+				isDown = (r == 1);
+			}
+		}
+		
+		String res = "";
+		for (int i = 0; i < tempStrings.length; i++) {
+			System.out.println("s: " + tempStrings[i]);
+			res += tempStrings[i];
+			
+		}
+		
+		System.out.println(res);
+		
+		return res;
+    }
+}
+
+
